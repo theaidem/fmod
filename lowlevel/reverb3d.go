45c5c47e4a70e3ce9f9389086956e43762308877
Max Kokorin <kokorin.max@gmail.com>
2015-08-19 18:02:40 +0600

    Docs for SoundGroup/Reverb3D API's

--- lowlevel/reverb3d.go
+++ lowlevel/reverb3d.go
@@ -13,6 +13,8 @@ type Reverb3D struct {
    'Reverb3D' API
 */
 
+// Releases the memory for a reverb object and makes it inactive.
+// If no reverb objects are created, the ambient reverb will be the only audible reverb. By default this ambient reverb setting is set to OFF.
 func (r *Reverb3D) Release() error {
 	res := C.FMOD_Reverb3D_Release(r.cptr)
 	return errs[res]
@@ -22,12 +24,34 @@ func (r *Reverb3D) Release() error {
    Reverb manipulation.
 */
 
+// Sets the 3d properties of a 'virtual' reverb object.
+//
+// position: Pointer to a vector containing the 3d position of the center of the reverb in 3d space. Default = { 0,0,0 }.
+//
+// mindistance: The distance from the centerpoint that the reverb will have full effect at. Default = 0.0.
+//
+// maxdistance: The distance from the centerpoint that the reverb will not have any effect. Default = 0.0.
+//
+//The 3D reverb object is a sphere having 3D attributes (position, minimum distance, maximum distance) and reverb properties.
+//
+// The properties and 3D attributes of all reverb objects collectively determine, along with the listener's position, the settings of and input gains into a single 3D reverb DSP.
+//
+// Please note that this only applies to software channels. When the listener is within the sphere of effect of one or more 3d reverbs, the listener's 3D reverb properties are
+// a weighted combination of such 3d reverbs. When the listener is outside all of the reverbs, the 3D reverb setting is set to the default ambient reverb setting.
 func (r *Reverb3D) Set3DAttributes(position Vector, mindistance, maxdistance float64) error {
 	cposition := position.toC()
 	res := C.FMOD_Reverb3D_Set3DAttributes(r.cptr, &cposition, C.float(mindistance), C.float(maxdistance))
 	return errs[res]
 }
 
+// Retrieves the 3d attributes of a Reverb object.
+//
+// The 3D reverb object is a sphere having 3D attributes (position, minimum distance, maximum distance) and reverb properties.
+//
+// The properties and 3D attributes of all reverb objects collectively determine, along with the listener's position, the settings of and input gains into a single 3D reverb DSP.
+//
+// Please note that this only applies to software channels. When the listener is within the sphere of effect of one or more 3d reverbs, the listener's 3D reverb properties are
+// a weighted combination of such 3d reverbs. When the listener is outside all of the reverbs, the 3D reverb setting is set to the default ambient reverb setting.
 func (r *Reverb3D) Get3DAttributes() (Vector, float64, float64, error) {
 	var cposition C.FMOD_VECTOR
 	var mindistance, maxdistance C.float
@@ -37,12 +61,17 @@ func (r *Reverb3D) Get3DAttributes() (Vector, float64, float64, error) {
 	return position, float64(mindistance), float64(maxdistance), errs[res]
 }
 
+// Sets reverb parameters for the current reverb object.
+// Reverb parameters can be set manually, or automatically using the pre-defined presets given in the fmod.h header.
+//
+// properties:  "ReverbProperties" structure which defines the attributes for the reverb
 func (r *Reverb3D) SetProperties(properties ReverbProperties) error {
 	cproperties := properties.toC()
 	res := C.FMOD_Reverb3D_SetProperties(r.cptr, &cproperties)
 	return errs[res]
 }
 
+// Retrieves the current reverb environment.
 func (r *Reverb3D) Properties() (ReverbProperties, error) {
 	var cproperties C.FMOD_REVERB_PROPERTIES
 	properties := NewReverbProperties()
@@ -51,11 +80,13 @@ func (r *Reverb3D) Properties() (ReverbProperties, error) {
 	return properties, errs[res]
 }
 
+// Disables or enables a reverb object so that it does or does not contribute to the 3d scene.
 func (r *Reverb3D) SetActive(active bool) error {
 	res := C.FMOD_Reverb3D_SetActive(r.cptr, getBool(active))
 	return errs[res]
 }
 
+// Retrieves the active state of the reverb object.
 func (r *Reverb3D) IsActive() (bool, error) {
 	var active C.FMOD_BOOL
 	res := C.FMOD_Reverb3D_GetActive(r.cptr, &active)
@@ -67,12 +98,18 @@ func (r *Reverb3D) IsActive() (bool, error) {
 */
 
 // NOTE: Not implement yet
+// Sets a user value that the Reverb object will store internally. Can be retrieved with "Reverb.UserData".
+// This function is primarily used in case the user wishes to 'attach' data to an FMOD object.
+//
+// It can be useful if an FMOD callback passes an object of this type as a parameter,
+// and the user does not know which object it is (if many of these types of objects exist). Using Reverb::getUserData would help in the identification of the object.
 func (r *Reverb3D) SetUserData(userdata *interface{}) error {
 	//FMOD_RESULT F_API FMOD_Reverb3D_SetUserData             (FMOD_REVERB3D *reverb3d, void *userdata);
 	return ErrNoImpl
 }
 
 // NOTE: Not implement yet
+// Retrieves the user value that that was set by calling the "Reverb.SetUserData" function.
 func (r *Reverb3D) UserData(userdata **interface{}) error {
 	//FMOD_RESULT F_API FMOD_Reverb3D_GetUserData             (FMOD_REVERB3D *reverb3d, void **userdata);
 	return ErrNoImpl
--- lowlevel/sound_group.go
+++ lowlevel/sound_group.go
@@ -13,11 +13,14 @@ type SoundGroup struct {
    'SoundGroup' API
 */
 
+// Releases a soundgroup object and returns all sounds back to the master sound group.
+// You cannot release the master sound group.
 func (s *SoundGroup) Release() error {
 	res := C.FMOD_SoundGroup_Release(s.cptr)
 	return errs[res]
 }
 
+// Retrieves the parent System object that was used to create this object.
 func (s *SoundGroup) SystemObject() (*System, error) {
 	var system System
 	res := C.FMOD_SoundGroup_GetSystemObject(s.cptr, &system.cptr)
@@ -28,11 +31,19 @@ func (s *SoundGroup) SystemObject() (*System, error) {
    SoundGroup control functions.
 */
 
+// Limits the number of concurrent playbacks of sounds in a sound group to the specified value.
+// After this, if the sounds in the sound group are playing this many times, any attepts to play more of the sounds in the sound group will by default fail with FMOD_ERR_MAXAUDIBLE.
+// Use "SoundGroup.SetMaxAudibleBehavior" to change the way the sound playback behaves when too many sounds are playing. Muting, failing and stealing behaviors can be specified.
+// "SoundGroup.NumPlaying" can be used to determine how many instances of the sounds in the sound group are currently playing.
+//
+// maxaudible: A variable to receive the number of playbacks to be audible at once. -1 = unlimited. 0 means no sounds in this group will succeed. Default = -1.
 func (s *SoundGroup) SetMaxAudible(maxaudible int) error {
 	res := C.FMOD_SoundGroup_SetMaxAudible(s.cptr, C.int(maxaudible))
 	return errs[res]
 }
 
+// Retrieves the number of concurrent playbacks of sounds in a sound group to the specified value.
+// If the sounds in the sound group are playing this many times, any attepts to play more of the sounds in the sound group will fail with FMOD_ERR_MAXAUDIBLE.
 func (s *SoundGroup) MaxAudible() (int, error) {
 	var maxaudible C.int
 	res := C.FMOD_SoundGroup_GetMaxAudible(s.cptr, &maxaudible)
@@ -40,39 +51,57 @@ func (s *SoundGroup) MaxAudible() (int, error) {
 }
 
 // NOTE: Not implement yet
+// This function changes the way the sound playback behaves when too many sounds are playing in a soundgroup. Muting, failing and stealing behaviors can be specified.
+// behavior: Specify a behavior determined with a FMOD_SOUNDGROUP_BEHAVIOR flag. Default is FMOD_SOUNDGROUP_BEHAVIOR_FAIL.
 func (s *SoundGroup) SetMaxAudibleBehavior(behavior C.FMOD_SOUNDGROUP_BEHAVIOR) error {
 	//FMOD_RESULT F_API FMOD_SoundGroup_SetMaxAudibleBehavior (FMOD_SOUNDGROUP *soundgroup, FMOD_SOUNDGROUP_BEHAVIOR behavior);
 	return ErrNoImpl
 }
 
 // NOTE: Not implement yet
+// Retrieves the current max audible behavior method.
 func (s *SoundGroup) MaxAudibleBehavior(behavior *C.FMOD_SOUNDGROUP_BEHAVIOR) error {
 	//FMOD_RESULT F_API FMOD_SoundGroup_GetMaxAudibleBehavior (FMOD_SOUNDGROUP *soundgroup, FMOD_SOUNDGROUP_BEHAVIOR *behavior);
 	return ErrNoImpl
 }
 
+// Specify a time in seconds for FMOD_SOUNDGROUP_BEHAVIOR_MUTE behavior to fade with. By default there is no fade.
+//
+// speed: Fade time in seconds (1.0 = 1 second). Default = 0.0. (no fade).
+//
+// When more sounds are playing in a SoundGroup than are specified with "SoundGroup.SetMaxAudible", the least important sound (ie lowest priority / lowest audible volume due to 3d position, volume etc)
+// will fade to silence if FMOD_SOUNDGROUP_BEHAVIOR_MUTE is used, and any previous sounds that were silent because of this rule will fade in if they are more important.
+//
+// If a mode besides FMOD_SOUNDGROUP_BEHAVIOR_MUTE is used, the fade speed is ignored.
 func (s *SoundGroup) SetMuteFadeSpeed(speed float64) error {
 	res := C.FMOD_SoundGroup_SetMuteFadeSpeed(s.cptr, C.float(speed))
 	return errs[res]
 }
 
+// Retrieves the current time in seconds for FMOD_SOUNDGROUP_BEHAVIOR_MUTE behavior to fade with.
+// If a mode besides FMOD_SOUNDGROUP_BEHAVIOR_MUTE is used, the fade speed is ignored.
 func (s *SoundGroup) MuteFadeSpeed() (float64, error) {
 	var speed C.float
 	res := C.FMOD_SoundGroup_GetMuteFadeSpeed(s.cptr, &speed)
 	return float64(speed), errs[res]
 }
 
+// Sets the volume for a sound group, affecting all channels playing the sounds in this soundgroup.
+//
+// volume: A linear volume level. 0.0 = silent, 1.0 = full volume. Default = 1.0. Negative volumes and amplification (> 1.0) are supported.
 func (s *SoundGroup) SetVolume(volume float64) error {
 	res := C.FMOD_SoundGroup_SetVolume(s.cptr, C.float(volume))
 	return errs[res]
 }
 
+// Retrieves the volume for the sounds within a soundgroup.
 func (s *SoundGroup) Volume() (float64, error) {
 	var volume C.float
 	res := C.FMOD_SoundGroup_GetVolume(s.cptr, &volume)
 	return float64(volume), errs[res]
 }
 
+// Stops all sounds within this soundgroup.
 func (s *SoundGroup) Stop() error {
 	res := C.FMOD_SoundGroup_Stop(s.cptr)
 	return errs[res]
@@ -83,23 +112,32 @@ func (s *SoundGroup) Stop() error {
 */
 
 // NOTE: Not implement yet
+// Retrieves the name of the sound group.
 func (s *SoundGroup) Name(name *C.char, namelen C.int) error {
 	//FMOD_RESULT F_API FMOD_SoundGroup_GetName(FMOD_SOUNDGROUP *soundgroup, char *name, int namelen);
 	return ErrNoImpl
 }
 
+// Retrieves the current number of sounds in this sound group.
 func (s *SoundGroup) NumSounds() (int, error) {
 	var numsounds C.int
 	res := C.FMOD_SoundGroup_GetNumSounds(s.cptr, &numsounds)
 	return int(numsounds), errs[res]
 }
 
+// Retrieves a pointer to a sound from within a sound group.
+//
+// index: Index of the sound that is to be retrieved.
+//
+// Use "SoundGroup.NumSounds" in conjunction with this function to enumerate all sounds in a sound group.
 func (s *SoundGroup) Sound(index int) (*Sound, error) {
 	var sound Sound
 	res := C.FMOD_SoundGroup_GetSound(s.cptr, C.int(index), &sound.cptr)
 	return &sound, errs[res]
 }
 
+// Retrieves the number of currently playing channels for the sound group.
+// This routine returns the number of channels playing. If the sound group only has 1 sound, and that sound is playing twice, the figure returned will be 2.
 func (s *SoundGroup) NumPlaying() (int, error) {
 	var numplaying C.int
 	res := C.FMOD_SoundGroup_GetNumPlaying(s.cptr, &numplaying)
@@ -111,12 +149,18 @@ func (s *SoundGroup) NumPlaying() (int, error) {
 */
 
 // NOTE: Not implement yet
+// Sets a user value that the SoundGroup object will store internally. Can be retrieved with "SoundGroup.UserData".
+//
+// This function is primarily used in case the user wishes to 'attach' data to an FMOD object.
+// It can be useful if an FMOD callback passes an object of this type as a parameter, and the user does not know which object it is (if many of these types of objects exist).
+// Using "SoundGroup.UserData" would help in the identification of the object.
 func (s *SoundGroup) SetUserData(userdata *interface{}) error {
 	//FMOD_RESULT F_API FMOD_SoundGroup_SetUserData           (FMOD_SOUNDGROUP *soundgroup, void *userdata);
 	return ErrNoImpl
 }
 
 // NOTE: Not implement yet
+// Retrieves the user value that that was set by calling the "SoundGroup.SetUserData" function.
 func (s *SoundGroup) UserData(userdata **interface{}) error {
 	//FMOD_RESULT F_API FMOD_SoundGroup_GetUserData           (FMOD_SOUNDGROUP *soundgroup, void **userdata);
 	return ErrNoImpl
